4ba9bc244cc9d748b2a25c2f90a03babb2d60dc1
Silva <sueliton.f.silva@accenture.com>
2021-01-28 16:18:49 -0300

    incluindo o titulo da pagina

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -27,6 +30,9 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - Modelo TCP/IP</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
