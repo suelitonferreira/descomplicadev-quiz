76ebd288d119dd623404eaec9bcd16b767bc08d5
Silva <sueliton.f.silva@accenture.com>
2021-01-28 16:10:27 -0300

    desafio da imagem ao compartilhar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
+import Head from 'next/head';
+
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -30,10 +33,18 @@ const theme = db.theme;
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <meta property="og:locale" content="pt_BR" />
+				<meta property="og:image" content={db.bg} />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" /> 
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
