6f497d50deadda7abe91ff371b38a69ced2b4ca4
Silva <sueliton.f.silva@accenture.com>
2021-01-28 16:27:34 -0300

    incluindo a teg da imagem

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,7 @@ export default function App({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         <meta property="og:locale" content="pt_BR" />
 				<meta property="og:image" content={db.bg} />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" /> 
       </Head>
