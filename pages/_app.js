7a79a10606efa803be1af33ff00d72bf8a5f9bff
Silva <sueliton.f.silva@accenture.com>
2021-01-31 15:21:50 -0300

    codigos desenvolvido na aula2 e aula3

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import db from '../db.json';
 import Head from 'next/head';
-
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -26,10 +25,11 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -40,20 +40,21 @@ export default function App({ Component, pageProps }) {
         <meta property="og:site_name" content="Quiz - Modelo TCP/IP [DescomplicaDev]" />
         <meta property="og:image:secure_url" content={db.bg} />
         <meta property="og:image:alt" content="Quiz-Modelo TCP/IP" />
-				<meta property="og:image" content={db.bg} />
+        <meta property="og:image" content={db.bg} />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" /> 
-        <meta property="twitter:title" content="Quiz - Modelo TCP/IP [DescomplicaDev]" /> 
-        <meta property="twitter:creator" content="@DescomplicaDev" /> 
-        <meta property="twitter:image" content={db.bg} /> 
-        <meta property="twitter:image:src" content={db.bg} /> 
-        <meta property="twitter:image:alt" content="Quiz-Modelo TCP/IP"/>
+        <meta property="og:image:height" content="600" />
+        <meta property="twitter:title" content="Quiz - Modelo TCP/IP [DescomplicaDev]" />
+        <meta property="twitter:creator" content="@DescomplicaDev" />
+        <meta property="twitter:image" content={db.bg} />
+        <meta property="twitter:image:src" content={db.bg} />
+        <meta property="twitter:image:alt" content="Quiz-Modelo TCP/IP" />
         <meta property="twitter:image:width" content="800" />
-        <meta property="twitter:image:height" content="600" /> 
+        <meta property="twitter:image:height" content="600" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import styled from 'styled-components'
-import Head from 'next/head'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -28,6 +31,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
@@ -41,6 +47,22 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
